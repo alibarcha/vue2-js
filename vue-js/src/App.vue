04e6ce30eb1f371d0112c9791a8031ef9dc893eb
Ali Haider <alibarcha910@gmail.com>
2022-06-06 21:04:25 -0700

    works on props

---
+++ vue-js/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+      <!-- root element -->
+  <div id="app">
+        <home-app data="This is prop data." msg="this is message one prop :)"></home-app>
+        <home-app msg="this is mseeage props"></home-app>
+  </div>
+</template>
+
+<script>
+    // import components
+  import HomeApp from './components/HomeApp.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    "home-app":HomeApp
+  }
+}
+</script>
+
+<style>
+    /* global styling */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ vue-js/src/components/HomeApp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>wellcome to vue2 js :) </h1>
+        <p>{{data}}</p>
+        <h2>{{msg}}</h2>
+    </div>
+
+
+</template>
+
+<script>
+  export default {
+      name:'HomeApp',
+      props : {
+          data:String,
+          msg:String,
+      }
+      
+  }
+
+</script>
+
+<style scoped>
+h1{
+    text-decoration-line: underline;
+    color: lightgreen ;
+}
+
+</style>
