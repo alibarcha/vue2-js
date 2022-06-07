51fbc1241558df2dbe00bcea1190045ba1709ad3
Ali Haider <alibarcha910@gmail.com>
2022-06-07 01:36:46 -0700

    v for loop

---
+++ vue-js-2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+   
+  <div id="app">
+       <HomeApp/>
+  </div>
+  
+
+</template>
+
+<script>
+   import HomeApp from './components/HomeApp.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HomeApp,
+
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
