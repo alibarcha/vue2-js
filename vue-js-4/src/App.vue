3713d71a1a92ca2fde22f17212da0a808580d7ff
Ali Haider <alibarcha910@gmail.com>
2022-06-07 18:45:47 -0700

    data binding

---
+++ vue-js-4/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+   
+  <div id="app">
+    <data-app></data-app>
+
+    <!-- prop component -->
+
+    <prop-app data="this is prop text" text="this is text props"></prop-app>
+     
+    
+  </div>
+
+</template>
+
+<script>
+  import DataBind from './components/DataBind.vue'
+  import PropsApp from './components/PropsApp.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    'data-app':DataBind,
+    'prop-app':PropsApp,
+    
+  
+  }
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+
+</style>
