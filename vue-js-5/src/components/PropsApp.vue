89e4f46aa890ff02fcf8977ef01cfb042a3bec9b
Ali Haider <alibarcha910@gmail.com>
2022-06-08 00:01:19 -0700

    props in vue js

---
+++ vue-js-5/src/components/PropsApp.vue
@@ -0,0 +1,35 @@
+<template>
+     <div id="container">
+         <!-- props data -->
+            <h2>user :{{name}}</h2>
+            <h2>client: {{client}}</h2>
+
+            <h3 v-for="user in users" :key="user.id">
+                {{user.name}}  --  {{user.email}}
+            </h3>
+     </div>
+</template>
+
+<script>
+export default {
+    name:'PropsApp',
+    data(){
+        return {
+           
+
+        }
+    },
+  
+    props:['name','client','users'],
+    methods:{
+
+    },
+    computed:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
