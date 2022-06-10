bed316ee3f2bfe1d9736295699108aea6239e9d6
Ali Haider <alibarcha910@gmail.com>
2022-06-10 02:02:53 -0700

    Fetch Data from APi

---
+++ vue-js-11/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+      <div id="app">
+        <h1>Fetch Data from API </h1>
+        <fetchApi/>
+        <FetchApp/>
+    
+       </div>
+</template>
+
+<script>
+
+import FetchApi from './components/FetchApi.vue'
+import FetchApp from './components/FetchApp.vue'
+export default {
+  components: {
+    FetchApi,
+    FetchApp
+  
+  },
+  data (){
+    return {
+     
+  
+    }
+  
+  },
+     methods : {
+     
+
+    }
+
+}
+
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
+}
+
+</style>
