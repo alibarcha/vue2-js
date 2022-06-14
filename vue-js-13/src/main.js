4c55963dc9665b50ce0126506dbe474a3517f401
Ali Haider <alibarcha910@gmail.com>
2022-06-13 21:01:15 -0700

    build 404 page

--- vue-js-13/src/main.js
+++ vue-js-13/src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 
 // router pkg
 import VueRouter from 'vue-router'
-// import components
+// import  components
 import Home from './views/Home'
 import About from './views/About'
 import Contact from './views/Contact'
 import  Login from './views/Login'
-
+import PageNotFound from './views/PageNotFound'
 
 Vue.use(VueRouter)
 
@@ -16,12 +16,10 @@ const routes=[
   {path:'/',component:Home},
   {path:'/about',component:About},
   {path:'/contact',component:Contact},
-  {path:'/login',component:Login}
-
+  {path:'/login',component:Login},
+  {path:'*' , component :PageNotFound}
 ]
-
 const router = new VueRouter({
-
   routes
 })
 
