bab4816b26a0eded055db04d690d5ce4c3135902
Ali Haider <alibarcha910@gmail.com>
2022-06-17 00:25:03 -0700

    slots in vue

---
+++ vue-js-16/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+      <div id="app">
+
+        <home-slot></home-slot>
+
+
+       </div>
+</template>
+
+<script>
+import HomeSlots from './components/HomeSlots.vue'
+
+export default {
+  name:"App",
+  components: {
+    'home-slot':HomeSlots,
+   
+   
+  },
+  data(){
+    return {
+      name:'william son'
+    
+    }
+  },
+  watch:{
+   
+}
+}
+
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
+ 
+}
+
+
+</style>
---
+++ vue-js-16/src/components/HomeSlots.vue
@@ -0,0 +1,85 @@
+<template>
+   <div id="home">
+      
+
+        <child-slot>
+            <!-- img -->
+            <template v-slot:imageBox>
+                <img src="../assets/Portfolio/01.jpg" alt="">
+            </template>
+            <!-- title -->
+            <template v-slot:title>
+                <h2>Special title treatment</h2>
+            </template>
+                <!-- content -->
+            <template v-slot:content>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, et in nulla qui nobis expedita.</p>
+                <a v-bind:href="link1">Learn More</a>
+            </template>
+        </child-slot>
+
+             <child-slot>
+            <!-- img -->
+            <template v-slot:imageBox>
+                <img src="../assets/Portfolio/03.jpg" alt="">
+            </template>
+            <!-- title -->
+            <template v-slot:title>
+                <h2>Special title </h2>
+            </template>
+                <!-- content -->
+            <template v-slot:content>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, et in nulla qui nobis expedita.</p>
+                 <a v-bind:href="link1">Learn More</a>
+            </template>
+        </child-slot>
+
+           <child-slot>
+            <!-- img -->
+            <template v-slot:imageBox>
+                <img src="../assets/Portfolio/02.jpg" alt="">
+            </template>
+            <!-- title -->
+            <template v-slot:title>
+                <h2>Special title treatment</h2>
+            </template>
+                <!-- content -->
+            <template v-slot:content>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, et in nulla qui nobis expedita.</p>
+                 <a v-bind:href="link1">Learn More</a>
+            </template>
+        </child-slot>
+
+   </div>
+</template>
+
+<script>
+import Slots from './Slots.vue'
+export default {
+    name:'HomeSlots',
+    components:{
+        'child-slot':Slots,
+    },
+    data(){
+        return {
+            link1:'https://vuejs.org/guide/essentials/watchers.html#stopping-a-watcher'
+        }
+    }
+
+
+}
+</script>
+
+<style scoped>
+ #home{
+    width: 90%;
+    margin: 20px auto;
+    border: 1px solid lightgreen;
+    /* background: #eee; */
+    padding: 30px;
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+ }
+
+</style>
