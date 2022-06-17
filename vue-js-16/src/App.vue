2352bfd3fd6a9e67bdcb4788b12c78cf7099371b
Ali Haider <alibarcha910@gmail.com>
2022-06-17 01:25:31 -0700

    Dynamic components render

--- vue-js-16/src/App.vue
+++ vue-js-16/src/App.vue
@@ -1,37 +1,53 @@
 <template>
-      <div id="app">
+  <div id="app">
+    <!-- <home-slot></home-slot> -->
+    <br />
+    <button v-on:click="comp = 'LoginForm'">Next</button>
+    <button v-on:click="comp = 'SignUp'">Default</button>
+    <button v-on:click="comp = 'PostData'">Previews</button>
+    <br /><br />
 
-        <home-slot></home-slot>
+    <!-- <component v-bind:is=tab /> -->
 
-
-       </div>
+    <component v-bind:is="comp" />
+  </div>
 </template>
 
 <script>
-import HomeSlots from './components/HomeSlots.vue'
+// import HomeSlots from './components/HomeSlots.vue'
+
+// import Java from './components/Java.vue'
+// import Node from './components/Node.vue'
+// import Php from './components/Php.vue'
+
+import LoginForm from "./components/LoginForm.vue";
+import SignUp from "./components/SignUp.vue";
+import PostData from "./components/PostData.vue";
 
 export default {
-  name:"App",
+  name: "App",
   components: {
-    'home-slot':HomeSlots,
-   
-   
-  },
-  data(){
-    return {
-      name:'william son'
-    
-    }
-  },
-  watch:{
-   
-}
-}
+    // 'home-slot':HomeSlots,
+    // Java,
+    // Node,
+    // Php
 
+    LoginForm,
+    SignUp,
+    PostData,
+  },
+  data() {
+    return {
+      // tab:'Java'
+      comp: "LoginForm",
+    };
+  },
+  watch: {},
+};
 </script>
 
-<style>
-*{
+<style scoped>
+* {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
@@ -40,8 +56,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
   text-align: center;
- 
 }
-
-
 </style>
